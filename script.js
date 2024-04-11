c0da88e9e609869ea97cab8a801bb812a766b69f
CSchechtman <craigschechtman@gmail.com>
2024-04-11 15:49:50 -0400

    calculator logic from eric

---
+++ script.js
@@ -0,0 +1,14 @@
+// let num1 = 100
+// let num2 = 5
+// let operator = "X"
+// let total = 500
+ 
+// click 9 -> assign num1 -> 9
+// click 2 -> assign num1 -> 92
+ 
+// + ->
+ 
+// click 5 -> because an operator exists -> assign num2
+ 
+// click = ->  do that operation with those numbers
+
