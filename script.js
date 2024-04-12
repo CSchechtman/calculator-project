faa1cba4202edfcb75a4a2530181f94ce1c59c0a
CSchechtman <craigschechtman@gmail.com>
2024-04-12 13:59:21 -0400

    Current Input is concat button inputs, working on switch for operators

--- script.js
+++ script.js
@@ -9,12 +9,44 @@
  // click 5 -> because an operator exists -> assign num2 
 // click = ->  do that operation with those numbers
 
+let num1 = null
+let num2 = null
 // Select all buttons with a number class
 const buttons = document.querySelectorAll('button[data-label]');
+const operators = document.querySelectorAll('button.operator');
+// Grab innertext(string) of Current Input, sets valueCurrentInput to the innertext
+let currentInput = document.querySelector(".currentInput");
+
+
+// let valueCurrentInput = parseInt(currentInputText);
+// console.log(typeof valueCurrentInput);
+// console.log(valueCurrentInput)
+// console.log(currentInput)
 
 // Add a click event listener to each number button
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        console.log(this.getAttribute('data-label'));
+        let buttonString = this.getAttribute('data-label');
+        let currentInputText = currentInput.innerText;
+// read innertext of Input. Change value of innertext to button just pressed. Grab output of innertext
+// and set the input to that value
+        if(currentInputText === "Current Input"){
+            currentInput.innerText = buttonString;
+            console.log(currentInputText);
+            console.log(currentInput.innerText);
+            num1 = parseInt(currentInput.innerText);
+            console.log(typeof(num1));
+        }else{
+            num1 = num1 + buttonString;
+            currentInput.innerText = num1;
+            console.log(typeof(num1));
+        }
     });
 });
+
+operators.forEach(button => {
+    button.addEventListener('click', function() {
+        console.log(this.getAttribute('data-label'));
+        let operator = this.getAttribute('data-label');
+    })
+})
