a51cf90456ba81bb30c6c703642195853f3c89bb
CSchechtman <craigschechtman@gmail.com>
2024-04-12 20:15:01 -0400

    working mvp, without styling

--- Calculator-v1/script.js
+++ Calculator-v1/script.js
@@ -3,10 +3,10 @@
 // let num2 = 5
 // let operator = "X"
 // let total = 500
- // click 9 -> assign num1 -> 9
+// click 9 -> assign num1 -> 9
 // click 2 -> assign num1 -> 92
- // + ->
- // click 5 -> because an operator exists -> assign num2 
+// + ->
+// click 5 -> because an operator exists -> assign num2 
 // click = ->  do that operation with those numbers
 
 let num1 = null
@@ -21,7 +21,7 @@ const buttons = document.querySelectorAll('button[data-label]');
 const operators = document.querySelectorAll('button[operator-label]');
 const allClear = document.querySelector('.allClear');
 const currentInput = document.querySelector(".currentInput");
-const equals = document.querySelector('#equals');
+const equals = document.querySelector('.equals');
 
 
 // let valueCurrentInput = parseInt(currentInputText);
@@ -31,59 +31,92 @@ const equals = document.querySelector('#equals');
 
 // Add a click event listener to every button with a data-label
 buttons.forEach(button => {
-    button.addEventListener('click', function() {
-        let buttonString = this.getAttribute('data-label');
-        let currentInputText = currentInput.innerText;
-// read innertext of Input. Change value of innertext to button just pressed. Grab output of innertext
-// and set the input to that value
-        if(currentInputText === "Current Input"){
-            currentInput.innerText = buttonString;
-            num1 = currentInput.innerText;
-        }else if(num1 !== null && operator !== null){
-            if(num2 === null){
-                num2 = buttonString;
-            }else{
-                num2 = num2 + buttonString;
+    button.addEventListener('click', function () {
+        if (subtotal === null) {
+            let buttonString = this.getAttribute('data-label');
+            let currentInputText = currentInput.innerText;
+            // read innertext of Input. Change value of innertext to button just pressed. Grab output of innertext
+            // and set the input to that value
+            if (currentInputText === "Current Input") {
+                currentInput.innerText = buttonString;
+                num1 = currentInput.innerText;
+            } else if (num1 !== null && operator !== null) {
+                if (num2 === null) {
+                    num2 = buttonString;
+                } else {
+                    num2 = num2 + buttonString;
+                }
+                currentInput.innerText = num1 + operator + num2;
+            } else {
+                num1 = num1 + buttonString;
+                currentInput.innerText = num1;
             }
-            currentInput.innerText = num1 + operator + num2;
-        // }else if(buttonString === "."){
-        //     currentInput.innerText = "0.";
-        }else{    
-            num1 = num1 + buttonString;
-            currentInput.innerText = num1;
         }
     });
 });
 
 operators.forEach(button => {
-    button.addEventListener('click', function() {
-        console.log(this.getAttribute('operator-label'));
-        if(operator === null){
-            operator = this.getAttribute('operator-label');
-            let currentInputText = currentInput.innerText;  
-            if(currentInputText === "Current Input"){
-                num1 = 0;
-                currentInput.innerText = "0" + operator;
-            }else{
-                num1 = currentInput.innerText;
-                currentInput.innerText = num1 + operator;
+    button.addEventListener('click', function () {
+        if (subtotal === null) {
+            console.log(this.getAttribute('operator-label'));
+            if (operator === null) {
+                operator = this.getAttribute('operator-label');
+                let currentInputText = currentInput.innerText;
+                if (currentInputText === "Current Input") {
+                    num1 = 0;
+                    currentInput.innerText = "0" + operator;
+                } else {
+                    num1 = currentInput.innerText;
+                    currentInput.innerText = num1 + operator;
+                }
             }
         }
     });
 });
 
+equals.addEventListener('click', function () {
+    if (subtotal === null) {
+        if (num1 !== null && operator !== null && num2 !== null) {
+            console.log("operator ", operator);
+            console.log("num1 ", num1);
+            console.log("num2 ", num2);
+            
+            num1 = parseFloat(num1);
+            num2 = parseFloat(num2);
+            console.log("num1 ", num1);
+            console.log("num2 ", num2);
+            switch (operator) {
+                case "+":
+                    subtotal = add(num1, num2);
+                    break;
+                case "-":
+                    subtotal = subtract(num1, num2);
+                    break;
+                case "/":
+                    subtotal = divide(num1, num2);
+                    break;
+                case "X":
+                    subtotal = multiply(num1, num2);
+                    break;
+            }
+            console.log("subtotal ", subtotal);
+            currentInput.innerText = subtotal;
+            currentInput.style.color = "red";
+        }
+    }
+});
 
-
-
-allClear.addEventListener('click', function() {
+allClear.addEventListener('click', function () {
     currentInput.innerText = 'Current Input';
     num1 = null;
     num2 = null;
     operator = null;
     subtotal = null;
+    currentInput.style.color = "black";
 });
 
 const add = (num1, num2) => {
+    console.log(typeof num1);
     return num1 + num2;
 };
 
@@ -91,7 +124,7 @@ const subtract = (num1, num2) => {
     return num1 - num2;
 };
 
-const multiply = (num1, num2) => { 
+const multiply = (num1, num2) => {
     return num1 * num2;
 };
 
